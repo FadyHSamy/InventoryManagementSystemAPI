aa98a1e8c1945846d6b2bb647d1c60080985a712
Fady Hany <fady.h.sammy@gmail.com>
2024-08-29 22:41:02 +0300

    Add project files.

---
+++ InventoryManagementSystem.Infrastructure/Context/DapperContext.cs
@@ -0,0 +1,34 @@
+﻿using InventoryManagementSystem.API.Configurations;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Infrastructure.Context
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public DapperContext(IOptions<DatabaseSettings> dbSettings)
+        {
+            _connectionString = dbSettings.Value.DefaultConnection;
+        }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+
+        // Optionally, implement a method to close the connection if needed
+        public void DisposeConnection(IDbConnection connection)
+        {
+            if (connection != null && connection.State != ConnectionState.Closed)
+            {
+                connection.Close();
+                connection.Dispose();
+            }
+        }
+    }
+}
---
+++ InventoryManagementSystem.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using InventoryManagementSystem.Infrastructure.Context;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddCustomServices(this IServiceCollection services)
+        {
+            // Register your services here
+            //services.AddScoped<IProductRepository, ProductRepository>();
+
+            // Register DapperContext
+            services.AddScoped<DapperContext>();
+
+        }
+    }
+}
